b99a132faa089953ff8739565999004cc533c5d1
Decibel <engineering@decibel.trade>
2026-01-23 16:17:35 -0500

    Project import generated by Copybara.
    GitOrigin-RevId: 31847fed70d4a84202a718884b4ab739e06272cb

--- src/read/market-contexts/market-contexts.types.ts
+++ src/read/market-contexts/market-contexts.types.ts
@@ -4,9 +4,6 @@ export const MarketContextSchema = z.object({
   market: z.string(),
   volume_24h: z.number(),
   open_interest: z.number(),
-  mark_price: z.number(),
-  mid_price: z.number(),
-  oracle_price: z.number(),
   previous_day_price: z.number(),
   price_change_pct_24h: z.number(),
 });
