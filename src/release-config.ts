4107f16889536a6af96685f9c19a6e8a2739e81b
Decibel <engineering@decibel.trade>
2026-02-10 13:45:13 -0800

    Project import generated by Copybara.
    GitOrigin-RevId: fadab698a9675a558bf72802510e59ef0925b527

--- src/release-config.ts
+++ src/release-config.ts
@@ -4,8 +4,8 @@ export enum CompatVersion {
   // V0_1 = "v0.1", // decibel-testnet-release-v0.1
   // V0_2_PARTIAL = "v0.2.partial", // decibel-testnet-release-v0.2
   // V0_2 = "v0.2", // decibel-release-v0.12
-  V0_3 = "v0.3", // decibel-testnet-release-v0.3
-  V0_4 = "v0.4", // Current main branch (bumped when new branch is created)
+  // V0_3 = "v0.3", // decibel-testnet-release-v0.3
+  V0_4 = "v0.4", // decibel-testnet-release-v0.4 - and final version.
 }
 
 export interface ReleaseConfig {
@@ -14,18 +14,18 @@ export interface ReleaseConfig {
 
 export const PACKAGE = {
   NETNA: "0xb8a5788314451ce4d2fbbad32e1bad88d4184b73943b7fe5166eab93cf1a5a95",
-  TESTNET: "0xd0b2dd565e0f2020d66d581a938e7766b2163db4b8c63410c17578d32b4e9e88",
+  TESTNET: "0x952535c3049e52f195f26798c2f1340d7dd5100edbe0f464e520a974d16fbe9f",
   MAINNET: "0xe6683d451db246750f180fb78d9b5e0a855dacba64ddf5810dffdaeb221e46bf",
 };
 
-export const DEFAULT_COMPAT_VERSION = CompatVersion.V0_3;
+export const DEFAULT_COMPAT_VERSION = CompatVersion.V0_4;
 
 const NETNA_RELEASE_CONFIG: ReleaseConfig = {
-  compatVersion: CompatVersion.V0_3,
+  compatVersion: CompatVersion.V0_4,
 };
 
 const TESTNET_RELEASE_CONFIG: ReleaseConfig = {
-  compatVersion: CompatVersion.V0_3,
+  compatVersion: CompatVersion.V0_4,
 };
 
 const LOCAL_RELEASE_CONFIG: ReleaseConfig = {
@@ -37,7 +37,7 @@ const DOCKER_RELEASE_CONFIG: ReleaseConfig = {
 };
 
 const MAINNET_RELEASE_CONFIG: ReleaseConfig = {
-  compatVersion: CompatVersion.V0_3,
+  compatVersion: CompatVersion.V0_4,
 };
 
 export const RELEASE_CONFIGS = {
